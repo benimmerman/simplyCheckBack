4a5151ad7a67dacbc4e3db2067379a759d07e948
benimmerman <ben.immerman@gmail.com>
2025-05-09 08:42:27 -0400

    Change character limits on items, notes, and titles

--- checklist_app/models/checklist.py
+++ checklist_app/models/checklist.py
@@ -5,15 +5,15 @@ from django.utils import timezone
 class Lists(models.Model):
     listId = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
-    listTitle = models.CharField(max_length=30, default='Checklist')
+    listTitle = models.CharField(max_length=100, default='Checklist')
     createdWhen = models.DateTimeField(default=timezone.now)
     lastModified = models.DateTimeField(null=True)
     lastModifiedBy = models.IntegerField(null=True)
 
 class ListItems(models.Model):
     listId = models.ForeignKey(Lists, on_delete=models.CASCADE)
-    itemName = models.CharField(max_length=40)
-    notes = models.CharField(null=True,max_length=100)
+    itemName = models.CharField(max_length=1000)
+    notes = models.CharField(null=True,max_length=4000)
     isDone = models.BooleanField(default=False)
     createdWhen = models.DateTimeField(default=timezone.now)
     createdBy = models.IntegerField(default=1)
